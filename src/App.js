bf500148270d18412f535121a7ac58499602f42d
unknown <kevinkuchar2378@gmail.com>
2018-08-05 13:44:27 -0400

    chore(init): adds react setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+//styles
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Test</div>
+    );
+  }
+}
+
+export default App;
